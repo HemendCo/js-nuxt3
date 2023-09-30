6f8386a143901107453826dffbd2a7006253e214
Balal Arast <arastbalal@gmail.com>
2023-09-30 05:29:27 +0330

    update package : @hemend/js-library@1.0.15

--- src/runtime/composables/useUser.ts
+++ src/runtime/composables/useUser.ts
@@ -2,29 +2,29 @@ import { useState, useNuxtApp } from '#imports'
 import { UserParams } from '../../types'
 
 export default function () {
-    const { $hemend } = useNuxtApp()
+  const { $hemend } = useNuxtApp()
 
-    let params = $hemend.storage().get('user');
+  let params = $hemend.storage().get('user');
 
-    const user = useState<UserParams|null>('user', () => (params || null))
-    
-    const getUser = () : UserParams|null => {
-        return user.value
-    }
+  const user = useState<UserParams|null>('user', () => (params || null))
+  
+  const getUser = () : UserParams|null => {
+    return user.value
+  }
 
-    const setUser = (params: UserParams) => {
-        $hemend.storage().set('user', params);
-        user.value = params
-    }
+  const setUser = (params: UserParams) => {
+    $hemend.storage().set('user', params);
+    user.value = params
+  }
 
-    const clearUserData = () => {
-        $hemend.storage().remove('user');
-        user.value = null
-    }
+  const clearUserData = () => {
+    $hemend.storage().remove('user');
+    user.value = null
+  }
 
-    return {
-        getUser,
-        setUser,
-        clearUserData
-    }
+  return {
+    getUser,
+    setUser,
+    clearUserData
+  }
 }
--- src/runtime/helpers/flashMessage.ts
+++ src/runtime/helpers/flashMessage.ts
@@ -3,34 +3,34 @@ import type { FlashMessageParams, FlashMessageOptions, FlashMessage } from '../.
 import { FlashMessagePlugin } from '@smartweb/vue-flash-message';
 
 const getFlashMessageParams = (options: FlashMessageOptions, params: FlashMessageParams, type?: 'error' | 'warning' | 'info' | 'success') => {
-    if(type) {
-        params.type = type;
-    }
-    if(params.rtl || (!('rtl' in params) && options.rtl)) {
-        params['blockClass'] = params['blockClass'] ? (params['blockClass'].rtrim() + ' rtl').trim() : 'rtl';
-    }
+  if(type) {
+    params.type = type;
+  }
+  if(params.rtl || (!('rtl' in params) && options.rtl)) {
+    params['blockClass'] = params['blockClass'] ? (params['blockClass'].rtrim() + ' rtl').trim() : 'rtl';
+  }
 
-    return params;
+  return params;
 }
 
 const flashMessage = (flashMessage: FlashMessagePlugin, options: FlashMessageOptions): FlashMessage => {
-    return {
-        show: (params: FlashMessageParams) => {
-          flashMessage.show(getFlashMessageParams(options, params))
-        },
-        error: (params: FlashMessageParams) => {
-          flashMessage.show(getFlashMessageParams(options, params, 'error'))
-        },
-        warning: (params: FlashMessageParams) => {
-          flashMessage.show(getFlashMessageParams(options, params, 'warning'))
-        },
-        info: (params: FlashMessageParams) => {
-          flashMessage.show(getFlashMessageParams(options, params, 'info'))
-        },
-        success: (params: FlashMessageParams) => {
-          flashMessage.show(getFlashMessageParams(options, params, 'success'))
-        },
-    }
+  return {
+    show: (params: FlashMessageParams) => {
+      flashMessage.show(getFlashMessageParams(options, params))
+    },
+    error: (params: FlashMessageParams) => {
+      flashMessage.show(getFlashMessageParams(options, params, 'error'))
+    },
+    warning: (params: FlashMessageParams) => {
+      flashMessage.show(getFlashMessageParams(options, params, 'warning'))
+    },
+    info: (params: FlashMessageParams) => {
+      flashMessage.show(getFlashMessageParams(options, params, 'info'))
+    },
+    success: (params: FlashMessageParams) => {
+      flashMessage.show(getFlashMessageParams(options, params, 'success'))
+    },
+  }
 }
 
 export default flashMessage;
